397d6e718c031481a6695b4f858eba66690d48d8
irenevg18 <irenevalleguillen@gmail.com>
2023-10-10 21:42:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-3xl font-bold underline text-emerald-900">Hello world!</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style></style>
